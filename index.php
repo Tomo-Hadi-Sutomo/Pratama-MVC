400a930f1bc31eccb7c46f8af680d613290c5dec
Tomo-Hadi-Sutomo <tomo.logos@gmail.com>
2020-03-07 07:38:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php /* TOMO - PRATAMA STUDIO */
 DECLARE(STRICT_TYPES=1);
 define('ROOT', dirname(__FILE__));
+define('START', microtime(true)); // comment this if you not using elapsed_time() function
 require 'App.php';
 new App();
