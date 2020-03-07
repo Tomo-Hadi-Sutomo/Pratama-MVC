a440ceeb6773c91ee814801738c876c4c0748496
Tomo-Hadi-Sutomo <tomo.logos@gmail.com>
2020-03-07 07:41:09 +0700

    Adding Database call

--- controllers/Index.php
+++ controllers/Index.php
@@ -1,11 +1,13 @@
 <?php /* TOMO - PRATAMA STUDIO */
 DECLARE(STRICT_TYPES=1);
 class Index {
-	public function __construct() {
-		//code start here
-	}
 	public function index() {
 		$data['title'] = "Halaman Index - Pratama MVC is Running!";
 		render('welcome', $data);
+	}
+	public function contoh_data() {
+		$data['absensi'] = mysqli_fetch_all(mysqli_query(APP::$db,'select * from absensi'), MYSQLI_ASSOC);
+		$data['title'] = "Contoh Halaman Database!";
+		render('index', $data, 'logos');
 	}
 }
